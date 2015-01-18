e4145322d7f567fe72953599b7c44b12c5aac6cc
Arkadiusz Sokolowski <a.sokolowski@ocado.com>
2015-01-18 17:11:04 +0000

    Initial commit

---
+++ src/main/java/org/areso/fordocker/ForDocker1Application.java
@@ -0,0 +1,12 @@
+package org.areso.fordocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForDocker1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForDocker1Application.class, args);
+    }
+}
---
+++ src/test/java/org/areso/fordocker/ForDocker1ApplicationTests.java
@@ -0,0 +1,16 @@
+package org.areso.fordocker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = ForDocker1Application.class)
+public class ForDocker1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
